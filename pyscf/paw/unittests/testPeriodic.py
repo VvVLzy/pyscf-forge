edf3ecd34fc88fb25a9821f45fe4cb56987d4d8f
Ziyao Luo <55331528+VvVLzy@users.noreply.github.com>
2025-10-24 11:33:43 -0700

    periodic code is working pretty well for single atom

--- pyscf/paw/unittests/testPeriodic.py
+++ pyscf/paw/unittests/testPeriodic.py
@@ -13,15 +13,16 @@ def setUpModule():
     global cell, init_guess, xc, auxbasis
 
     L = 3.  # box size
-    x = L/2 # atom in the center
+    # x = L/2 # atom in the center
+    x = 0
 
     atom =  f"""
     He      {x} {x} {x}
     """
-    basis = "cc-pvtz"
+    basis = "ccpvtz"
     verbose = 3
     a = numpy.eye(3) * L
-    ke_cutoff = 100
+    ke_cutoff = 200
 
     cell = pgto.M(
         atom        = atom,
@@ -29,6 +30,7 @@ def setUpModule():
         verbose     = verbose,
         a           = a,
         ke_cutoff   = ke_cutoff,
+        precision   = 1e-5
     )
 
     # TODO: the default minao guess usually give mo that is not (cell.nao, cell.nao)
@@ -52,11 +54,18 @@ class testPAW(unittest.TestCase):
         mf_per_rks.kernel()
         e_ref = mf_per_rks.e_tot/cell.natm
 
+        # # Reference FFTDF calculation
+        # mf_per_rks_fftdf = pscf.RKS(cell)
+        # mf_per_rks_fftdf.init_guess = init_guess
+        # mf_per_rks_fftdf.xc = xc
+        # mf_per_rks_fftdf.kernel()
+        # e_ref = mf_per_rks_fftdf.e_tot/cell.natm
+
         # PAW calculation
         mf_per_rks_paw = pscf.RKS(cell)
         mf_per_rks_paw.init_guess = init_guess
         mf_per_rks_paw.xc = xc
-        mydf = PAW.from_mf(mf_per_rks_paw, PWAccuracy=1e-4).build()
+        mydf = PAW.from_mf(mf_per_rks_paw, PWAccuracy=1e-8).build()
         mf_per_rks_paw.with_df = mydf
         
         mf_per_rks_paw.kernel()
@@ -65,6 +74,10 @@ class testPAW(unittest.TestCase):
         # # Check convergence and energy agreement
         # self.assertTrue(mf_per_rks_paw.converged)
         # self.assertAlmostEqual(e_ref, e_occri, places=3)
+
+        # Done: compare original GAPW scf and pyscf periodic to see if error is similar
+        # the current GAPW is more accurate than GAPW before (Yes!)
+
 
 
 if __name__ == "__main__":
