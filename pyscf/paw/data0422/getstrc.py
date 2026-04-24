dfbd14cb9ae3493d1547c81690725ba580b609ef
Ziyao Luo <55331528+VvVLzy@users.noreply.github.com>
2026-04-24 14:36:12 -0700

    remove presentation data

--- pyscf/paw/data0422/getstrc.py
+++
@@ -1,9 +0,0 @@
-from mp_api.client import MPRester
-
-# Use your Materials Project API key
-with MPRester("ld5HdyuSrVhvlNZKJIPy2iM58UCKuX2V") as mpr:
-    # get_structure_by_material_id returns the primitive cell by default
-    primitive_struct = mpr.get_structure_by_material_id("mp-66")
-    
-    # Export to CIF for use in other software
-    primitive_struct.to(filename="C_primitive.cif")
